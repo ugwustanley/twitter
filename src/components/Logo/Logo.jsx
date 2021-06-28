ce1de067925031b33ef30e31c437148e4d0958b7
ikxyz <40415727+Ikxyz@users.noreply.github.com>
2021-06-28 18:09:07 +0100

    inital commit

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import logoSvg from "../../assets/images/twitter.svg";
+
+export default function Logo() {
+     return <img src={logoSvg} width="100" height="100" alt="app logo" />
+}
---
+++ src/pages/App/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from "../../components/Logo/Logo";
+import "./App.scss";
+
+
+export default function App() {
+
+     return <>
+
+          <div className="center">
+               <Logo />
+          </div>
+
+
+     </>
+}
